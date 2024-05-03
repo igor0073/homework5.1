cb7a3d56945f67d93d72603ddeb59b07ba52cd1e
igor0073 <166950497+igor0073@users.noreply.github.com>
2024-05-03 22:12:06 +0300

    Add files via upload

---
+++ homwerk5.py
@@ -0,0 +1,14 @@
+my_list = ['apple','pear','strawberry','cherry', 'wattermelon', 'peach' ]
+print(my_list)
+print(my_list[0])
+print(my_list[5])
+print(my_list[2:5])
+my_list [2] = 'banana'
+print(my_list)
+
+
+my_dict = {'apple': 'яблокко', 'pear': 'груша', 'strawberry': 'клубника','cherry': 'вишня','wattermelon': 'арбуз','peach': 'персик'}
+print(my_dict)
+print(my_dict['cherry'])
+my_dict['pear'] = 'не груша'
+print(my_dict)
